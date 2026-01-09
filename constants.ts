75dc3e1b88fe69e64c95205d90ee0b229f36fe1b
Current Zeng <astro.current.z@gmail.com>
2026-01-09 13:36:15 -0800

    image fixes

--- constants.ts
+++ constants.ts
@@ -8,7 +8,7 @@ export const ARTWORKS: Artwork[] = [
     title: 'THE_APEX_ARC',
     description: 'Sever the noise. Lethal focus.',
     // Path starts with /assets because 'public' is the root folder for the browser
-    imageUrl: 'assets/images/fire series-01.png', 
+    imageUrl: 'assets/images/fire_series-01.png', 
     type: 'graphic',
     year: '2026'
   },
@@ -16,7 +16,7 @@ export const ARTWORKS: Artwork[] = [
     id: 'fire_2', // Represents Passion/Visuality
     title: 'THE_CORE_CRUCIBLE',
     description: 'Pain into fuel. Internal combustion.',
-    imageUrl: 'assets/images/fire series-02.png',
+    imageUrl: 'assets/images/fire_series-02.png',
     type: 'motion',
     year: '2026'
   },
@@ -24,7 +24,7 @@ export const ARTWORKS: Artwork[] = [
     id: 'fire_3', // Represents Structure/Code
     title: 'PROTOCOL_WILDFILRE',
     description: 'Total release. Uncontained energy.',
-    imageUrl: 'assets/images/fire series-03.png',
+    imageUrl: 'assets/images/fire_series-03.png',
     type: 'graphic',
     year: '2026'
   }
